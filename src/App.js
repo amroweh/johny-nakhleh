4f6e321eb11823d71c362848d4b4ee63c8bc344f
Ali Mroweh <amroweh@gmail.com>
2023-09-10 23:33:49 +0100

    added dummy image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div style={{textAlign: "center"}}>
       <h1>Hello from Johny's new Site!</h1>
       <h3>Johny is a great personal trainer!</h3>
-    </>
+      <img
+        style={{ width: "200px" }}
+        src={process.env.PUBLIC_URL + "/johnyanddog.png"}
+      ></img>
+    </div>
   );
 }
 
