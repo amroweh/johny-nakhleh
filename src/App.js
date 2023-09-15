bd25a23ac20e8a6289ad33ca19f173bba4b55572
Ali Mroweh <amroweh@gmail.com>
2023-09-15 23:00:36 +0100

    using yarn and testing headers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import "./App.css";
+import Header2 from "./components/Header2";
+import "./css/globals.css";
+// import Header from "./components/Header";
 
 function App() {
   return (
-    <div style={{textAlign: "center"}}>
-      <h1>Hello from Johny's new Site!</h1>
-      <h3>Johny is a great personal trainer!</h3>
-      <img
-        style={{ width: "200px" }}
-        src={process.env.PUBLIC_URL + "/johnyanddog.png"}
-      ></img>
-    </div>
+    <>
+      {/* <Header /> */}
+      <Header2 />
+      <div style={{ textAlign: "center" }}>
+        <h1>Hello from Johny's new Site!</h1>
+        <h3>Johny is a great personal trainer!</h3>
+        <img
+          style={{ width: "200px" }}
+          src={process.env.PUBLIC_URL + "/johnyanddog.png"}
+        ></img>
+      </div>
+    </>
   );
 }
 
